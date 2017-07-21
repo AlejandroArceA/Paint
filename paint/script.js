bf01a3468509c78d0f559d9447716534c7672519
AlejandroArceA <arce.gmns@gmail.com>
2017-07-21 17:10:32 -0500

    avance 3

--- paint/script.js
+++ paint/script.js
@@ -5,10 +5,16 @@ function iniciar(){
        var dibujar;
        var canvas1=document.getElementById('canvas');
        var contexto=canvas1.getContext('2d');
+       var grosor;
        
        $('#canvas').on('mousedown',presionar);
        $('#canvas').on('mousemove',pintar);
        $('#canvas').on('mouseup',dejar);
+       $('#cambio button').on('click',function(){
+
+       	   var g=$(this).text();
+       	   grosor=g;
+       })
 
        function presionar(){
 
@@ -20,6 +26,10 @@ function iniciar(){
 
        	     if(dibujar){
 
+                 contexto.beginPath();
+                 contexto.fillStyle="blue";
+                 contexto.arc(event.clientX,event.clientY,grosor,0,360);
+                 contexto.fill();
        	     	//contexto.lineTo(event.clientX,event.clientY);
        	     	//contexto.stroke();
        	     }
